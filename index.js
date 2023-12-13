bbdd0e5f9748113324de752be805ff9509bce82a
tretzer32 <princ2019i@gmail.com>
2023-12-13 23:59:06 +0200

    Add navigation

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-const burger = document.querySelector('.header__burger');
-const header__nav = document.querySelector('.header__nav');
-const body = document.querySelector('body');
+    const burger = document.querySelector('.header__burger');
+    const header__nav = document.querySelector('.header__nav');
+    const body = document.querySelector('body');
+    const header__links = document.querySelectorAll('.menu__link[data-goto]');
+    const header = document.querySelector('.header');
 
-burger.addEventListener('click', ()=>{
 
-    burger.classList.toggle('active');
-    header__nav.classList.toggle('active');
-    body.classList.toggle('lock');
-});
+    burger.addEventListener('click', () => {
+
+        burger.classList.toggle('active');
+        header__nav.classList.toggle('active');
+        body.classList.toggle('lock');
+    });
+
+
+
+
+    if (header__links.length > 0) {
+
+        header__links.forEach(menu__link => {
+
+            menu__link.addEventListener('click', makeScroll);
+        })
+    };
+
+    function makeScroll(e) {
+
+        const menu__link = e.target;
+        if (menu__link.dataset.goto && document.querySelector(menu__link.dataset.goto)) {
+
+            // menu__link.dataset.goto - значення атрибута, яке містить інформацію про те, куди слід прокручувати сторінку
+            // document.querySelector(menu__link.dataset.goto) - сам блок на який потрібно перейти
+
+            const gotoBlock = document.querySelector(menu__link.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY;
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            if (burger.classList.contains('active')) {
+
+                    burger.classList.remove('active');
+                    header__nav.classList.remove('active');
+                    body.classList.remove('lock');
+            }
+        }
+        e.preventDefault();
+    }
+
+
 
 });
